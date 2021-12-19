2f9ceb5ee92e6d95be26f2dd838526f248ee0f12
liuzhen <1261964083@qq.com>
2021-12-19 14:48:03 +0800

    暂时全部删光

--- src/main/java/com/h3c/mapper/BookMapper.java
+++
@@ -1,7 +0,0 @@
-package com.h3c.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.h3c.entity.Book;
-
-public interface BookMapper extends BaseMapper<Book> {
-}
--- src/main/java/com/h3c/service/impl/BookServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.h3c.service.impl;
-
-import com.h3c.service.BookService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class BookServiceImpl implements BookService {
-}
